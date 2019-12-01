af9c8349af7d7641b83d9c08e2c174aa77016117
peterwangsb <57841638+peterwangsb@users.noreply.github.com>
2019-11-30 20:55:36 -0500

    Add files via upload

---
+++ HW3_1.py
@@ -0,0 +1,7 @@
+cities = ["NYC","LA","SF"]
+small_cities = ["Stony Brook","Port Jeff"]
+city1 = small_cities[0]
+city2 = small_cities[1]
+cities.insert(2,city1)
+cities.insert(3,city2)
+print(cities);
